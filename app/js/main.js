2a88de0156a17c57dec21abd597b92d3637a42b3
YanG-6668 <goch.yannn@gmail.com>
2020-02-17 15:44:18 +0200

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
-$(function(){
+$(function() {
+
+
+
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slick-arrow slick-prev"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"></button>'
+    });
+
+
 
 });

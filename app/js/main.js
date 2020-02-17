06dd1dd5bda012823cb695173702323be24182fa
YanG-6668 <goch.yannn@gmail.com>
2020-02-17 23:39:41 +0200

    added two sections

--- app/js/main.js
+++ app/js/main.js
@@ -9,6 +9,15 @@ $(function() {
         nextArrow: '<button class="slick-arrow slick-next"></button>'
     });
 
+    var mixer = mixitup('.products__inner-box');
 
+    $('.slider__second-inner').slick({
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        prevArrow: '<button class="slick-arrow slick-prev"></button>',
+        nextArrow: '<button class="slick-arrow slick-next"></button>',
+
+    });
 
 });
